3d49d3d58a980c6b324aa89469dc6a1ba0daf221
cryzz <tyu12@hotmail.es>
2024-04-18 01:59:49 -0500

    #ultimo cambio

--- caltamiranoT3/Vistas/vLogin.xaml.cs
+++ caltamiranoT3/Vistas/vLogin.xaml.cs
@@ -1,41 +1,35 @@
 namespace caltamiranoT3.Vistas;
-
 public partial class vLogin : ContentPage
 {
     string[] Usuarios = { "Carlos", "Ana", "Jose" };
     string[] Contrasenas = { "carlos123", "ana123", "jose123" };
-
     public vLogin()
 	{
-    
-
         InitializeComponent();
 	}
-
     private void btnIniciar_Clicked(object sender, EventArgs e)
     {
-        string usuarioInput = txtUsuario.Text;
-        string contrasenaInput = txtContrasena.Text;
+        string cajaUsuario = txtUsuario.Text;
+        string cajaContrasena = txtContrasena.Text;
 
         bool usuarioValido = false;
         bool contrasenaValida = false;
 
         for (int i = 0; i < Usuarios.Length; i++)
         {
-            if (Usuarios[i] == usuarioInput)
+            if (Usuarios[i] == cajaUsuario)
             {
                 usuarioValido = true;
-                if (Contrasenas[i] == contrasenaInput)
+                if (Contrasenas[i] == cajaContrasena)
                 {
                     contrasenaValida = true;
                     break;
                 }
             }
         }
-
         if (usuarioValido && contrasenaValida)
         {
-            Navigation.PushAsync(new vPrincipal(usuarioInput));
+            Navigation.PushAsync(new vPrincipal(cajaUsuario));
         }
         else
         {
--- caltamiranoT3/Vistas/vPrincipal.xaml.cs
+++ caltamiranoT3/Vistas/vPrincipal.xaml.cs
@@ -2,11 +2,11 @@ namespace caltamiranoT3.Vistas;
 
 public partial class vPrincipal : ContentPage
 {
-	public vPrincipal(string usuarioInput)
+	public vPrincipal(string cajaUsuario)
 	{
 		InitializeComponent();
-        DisplayAlert("Alerta","Bienvenid@ " +usuarioInput, "Cerrar");
-        lblUsuario.Text = "Usuari@ conectad@ " + usuarioInput;
+        DisplayAlert("Alerta","Bienvenid@ " +cajaUsuario, "Cerrar");
+        lblUsuario.Text = "Usuari@ conectad@ " + cajaUsuario;
     }
 
   
